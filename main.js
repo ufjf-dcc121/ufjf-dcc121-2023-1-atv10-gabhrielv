ed1b4e6208e7ea14d931edf290fb15e846456957
Gabhriel Velasco <gabhriel.velasco@estudante.ufjf.br>
2023-05-30 19:31:26 -0300

    adiciona um estado básico

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 const form = document.forms.entrada;
-        form.addEventListener('submit', envia);
-        function envia(evento){
-            evento.preventDefault();
-            console.log('Formulário enviado!');
-        }
+form.addEventListener('submit', envia);
+
+let estado = 0;
+
+function envia(evento){
+    evento.preventDefault();
+    console.log('Formulário enviado!');
+    estado++;
+    atualiza();
+}
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
+
+}
